bbb2e7b327c7a934c4b84b2ca84777e0567c6969
Doriano <doriano.bergamin18@gmail.com>
2024-02-26 09:58:02 +1100

    added the first click element

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-document.getElementsByClassName("aboutus").addEventlistener("click");
-document.getElementsByClassName("breaky").addEventlistener("click");
-document.getElementsByClassName("breaky").addEventlistener("click");
-document.getElementsByClassName("lunch").addEventlistener("click");
-document.getElementsByClassName("drinks").addEventlistener("click");
+const aboutusClick = document.querySelector(".aboutus");
+const menuContainer = document.getElementById("menuContainer");
+
+aboutusClick.addEventListener("click", function () {
+  displayaboutUs(aboutUs);
+});
+function displayaboutUs(aboutus) {
+  menuContainer.innerHTML = "";
+
+  for (let menuItem of aboutus) {
+    const listItem = document.createElement("div");
+
+    listItem.innerHTML = `
+    ${menuItem.story}  
+    `;
+    menuContainer.appendChild(listItem);
+  }
+}
+
+const aboutUs = [
+  {
+    story:
+      "Welcome to The <strong>Green Leaf Cafe</strong>! Nestled in our vibrant cafe now proudly serves freshly brewed coffee sourced directly from the lush landscapes of Colombia. Immerse yourself in the rich flavors and aromatic notes of our carefully selected beans. Join us at our welcoming space, where the essence of Colombian coffee meets the cozy atmosphere of the Green Leaf Cafe. Experience the perfect blend of nature and indulgence in every cup at our latest venture.",
+  },
+];
 document.getElementsByClassName("lopue").addEventlistener("click");
 document.getElementsByClassName("fa").addEventlistener("click");
 document.getElementById("submit").addEventlistener("click");
