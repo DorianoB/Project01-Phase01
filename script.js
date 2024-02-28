e5a60a987a0bcbec82e2981e9a494e1c69baed52
Doriano <doriano.bergamin18@gmail.com>
2024-02-28 20:08:31 +1100

    clenaed code and added more objects

--- script.js
+++ script.js
@@ -5,35 +5,185 @@ let isBreakyOpen = false;
 let isLunchOpen = false;
 let isDrinksOpen = false;
 
+let menus = [
+  {
+    menuType: "breakfast",
+    openingHours: "7am - 11am",
+    items: [
+      {
+        food: "Banana Bread",
+        items: ["banana", "flour", "eggs"],
+        isVegan: false,
+        isGlutenfree: true,
+        price: 8.5,
+      },
+      {
+        food: "Breaky Roll",
+        items: ["eggs", "bacon", "cheese"],
+        isVegan: false,
+        isGlutenfree: false,
+        price: 15,
+      },
+      {
+        food: "Acai Bowl",
+        items: ["acai", "pineapple", "granola"],
+        isVegan: true,
+        isGlutenfree: true,
+        price: 17.5,
+      },
+    ],
+  },
+  {
+    menuType: "lunch",
+    openingHours: "11am - 3pm",
+    items: [
+      {
+        food: "Pizza",
+        items: ["dough", "tomato", "mushrooms", "vegan mozarella"],
+        isVegan: true,
+        isGlutenfree: false,
+        price: 20,
+      },
+      {
+        food: "Chicken Curry",
+        items: ["rice", "currypaste", "chicken"],
+        isVegan: false,
+        isGlutenfree: true,
+        price: 18,
+      },
+      {
+        food: "Sourdough Sandwich",
+        items: ["ham", "cheese", "pumpkin"],
+        isVegan: false,
+        isGlutenfree: false,
+        price: 14.2,
+      },
+    ],
+  },
+  {
+    menuType: "dinner",
+    openingHours: "6pm - 10pm",
+    items: [
+      {
+        food: "Pizza",
+        items: ["dough", "tomato", "mushrooms", "vegan mozarella"],
+        isVegan: true,
+        isGlutenfree: false,
+        price: 20,
+      },
+      {
+        food: "Pesto Prawns Pasta",
+        items: ["prawns", "spaghetti", "pesto"],
+        isVegan: false,
+        isGlutenfree: true,
+        price: 26,
+      },
+      {
+        food: "Chicken Parmy",
+        items: ["ham", "cheese", "chicken", "fries"],
+        isVegan: false,
+        isGlutenfree: false,
+        price: 21,
+      },
+    ],
+  },
+  {
+    menuType: "dessert",
+    openingHours: "6pm - 10pm",
+    items: [
+      {
+        food: "Choccolate Cacke",
+        items: ["choccolate", "strawberries"],
+        isVegan: true,
+        isGlutenfree: false,
+        price: 12,
+      },
+      {
+        food: "Ice Cream",
+        items: ["vanilla", "choccolate", "strawberries"],
+        isVegan: false,
+        isGlutenfree: true,
+        price: 6.5,
+      },
+      {
+        food: "Banana Split",
+        items: ["banana", "vanilla ice", "choccolate ice"],
+        isVegan: false,
+        isGlutenfree: false,
+        price: 12.5,
+      },
+    ],
+  },
+  {
+    menuType: "drinks",
+    openingHours: "All day",
+    items: [
+      {
+        cafe: "Flat White",
+        size: ["small", "medium", "large"],
+        price: [4.5, 5.5, 6.5],
+        extras: ["soya milk", " almond milk", " oat milk"],
+      },
+      {
+        cafe: "Cappuccino",
+        size: ["small", "medium", "large"],
+        price: [4.5, 5.5, 6.5],
+        extras: ["soya milk", " almond milk", "oat milk"],
+      },
+      {
+        cafe: "Long Black",
+        size: ["small", "medium", "large"],
+        price: [4.5, 5.5, 6.5],
+        extras: ["soya milk", " almond milk", " oat milk"],
+      },
+    ],
+  },
+];
+
 aboutusClick.addEventListener("click", function () {
   displayaboutUs();
 });
+
 function displayaboutUs() {
   aboutUsContainer.style.display = "block";
   menuContainer.style.display = "none";
-  // for (let menuItem of aboutus) {
-  //   const listItem = document.createElement("div");
-  //   listItem.innerHTML = `
-  //   ${menuItem.story}
-  //   `;
-  //   menuContainer.appendChild(listItem);
-  // }
 }
 
-const lopueClick = document.querySelector(".lopue");
-const cart = document.querySelector(".fa");
+// const lopueClick = document.querySelector(".lopue");
+// const cart = document.querySelector(".fa");
 
 const breakfast = document.querySelector(".breaky");
 breakfast.addEventListener("click", function () {
-  displayBreakyMenu();
+  displayMenu("breakfast");
 });
 
-function displayBreakyMenu() {
+const lunch = document.querySelector(".lunch");
+lunch.addEventListener("click", function () {
+  displayMenu("lunch");
+});
+
+const dinner = document.querySelector(".dinner");
+dinner.addEventListener("click", function () {
+  displayMenu("dinner");
+});
+
+const dessert = document.querySelector(".dessert");
+dessert.addEventListener("click", function () {
+  displayMenu("dessert");
+});
+
+function displayMenu(menuType) {
   menuContainer.style.display = "block";
   aboutUsContainer.style.display = "none";
   menuContainer.innerHTML = "";
 
-  for (let menuItem of breakyMenu) {
+  const menu = menus.filter((menu) => {
+    return menu.menuType === menuType;
+  });
+
+  console.log(menu);
+
+  for (let menuItem of menu[0].items) {
     const listItem = document.createElement("div");
 
     listItem.innerHTML = `
@@ -52,89 +202,6 @@ function displayBreakyMenu() {
     menuContainer.appendChild(listItem);
   }
 }
-const breakyMenu = [
-  {
-    food: "Banana Bread",
-    items: ["banana", "flour", "eggs"],
-    type: "Breakfast",
-    isVegan: false,
-    isGlutenfree: true,
-    price: 8.5,
-  },
-  {
-    food: "Breaky Roll",
-    items: ["eggs", "bacon", "cheese"],
-    type: "Breakfast",
-    isVegan: false,
-    isGlutenfree: false,
-    price: 15,
-  },
-  {
-    food: "Acai Bowl",
-    items: ["acai", "pineapple", "granola"],
-    type: "Breakfast",
-    isVegan: true,
-    isGlutenfree: true,
-    price: 17.5,
-  },
-];
-
-const lunch = document.querySelector(".lunch");
-lunch.addEventListener("click", function () {
-  displayLunchMenu();
-});
-
-function displayLunchMenu() {
-  menuContainer.style.display = "block";
-  aboutUsContainer.style.display = "none";
-  menuContainer.innerHTML = "";
-
-  for (let lunchItem of lunchMenu) {
-    const listItem = document.createElement("div");
-
-    listItem.innerHTML = `
-    <table>
-    <tr>
-    <th><strong>${lunchItem.food}${
-      lunchItem.isVegan ? " (V)" : ""
-    }</strong></th>
-   </tr>
-   <tr>
-   <tr><td>${lunchItem.items.join(", ")}</td></tr>
-   <tr><td>$${lunchItem.price}</td></tr>
-    </tr>
-    </table>
-    `;
-
-    menuContainer.appendChild(listItem);
-  }
-}
-const lunchMenu = [
-  {
-    food: "Pizza",
-    items: ["dough", "tomato", "mushrooms", "vegan mozarella"],
-    type: "Lunch",
-    isVegan: true,
-    isGlutenfree: false,
-    price: 20,
-  },
-  {
-    food: "Chicken Curry",
-    items: ["rice", "currypaste", "chicken"],
-    type: "Lunch",
-    isVegan: false,
-    isGlutenfree: true,
-    price: 18,
-  },
-  {
-    food: "Sourdough Sandwich",
-    items: ["ham", "cheese", "pumpkin"],
-    type: "Lunch",
-    isVegan: false,
-    isGlutenfree: false,
-    price: 14.2,
-  },
-];
 
 const drinks = document.querySelector(".drinks");
 drinks.addEventListener("click", function () {
@@ -146,7 +213,11 @@ function displayDrinksMenu() {
   aboutUsContainer.style.display = "none";
   menuContainer.innerHTML = "";
 
-  for (let drink of drinksMenu) {
+  const drinksMenu = menus.filter((menu) => {
+    return menu.menuType === "drinks";
+  });
+
+  for (let drink of drinksMenu[0].items) {
     const listItem = document.createElement("div");
     listItem.innerHTML = `
     <table>
@@ -162,27 +233,6 @@ function displayDrinksMenu() {
     menuContainer.appendChild(listItem);
   }
 }
-
-const drinksMenu = [
-  {
-    cafe: "Flat White",
-    size: ["small", "medium", "large"],
-    price: [4.5, 5.5, 6.5],
-    extras: ["soya milk", " almond milk", " oat milk"],
-  },
-  {
-    cafe: "Cappuccino",
-    size: ["small", "medium", "large"],
-    price: [4.5, 5.5, 6.5],
-    extras: ["soya milk", " almond milk", "oat milk"],
-  },
-  {
-    cafe: "Long Black",
-    size: ["small", "medium", "large"],
-    price: [4.5, 5.5, 6.5],
-    extras: ["soya milk", " almond milk", " oat milk"],
-  },
-];
 
 //email submiting form
 const submitClick = document.querySelector("#submit");
